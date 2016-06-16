1d858c923031fc06d4f9e584d8f0f3cf20f1a7a0
Andre Eisenbach <eisenbach@google.com>
2016-06-16 13:35:23 -0700

    Enable WBS for Oneplus3
    Bug: 28851833
    Change-Id: I29907b5eabef5420abfcc56fd6595d915b8b10c3

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -45,4 +45,15 @@ static inline const char* BtmGetDefaultName()
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+
+// Wide-band speech support
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
+
+// Google VSC spec support
+#define BLE_VND_INCLUDED TRUE
+
+// QCOM power management workaround
+#define BT_CLEAN_TURN_ON_DISABLED TRUE
+
 #endif
