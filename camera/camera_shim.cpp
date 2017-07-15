212ce8b703e1d99d17e8f1034f3fe44f3a3cce7c
Sultanxda <sultanxda@gmail.com>
2017-07-15 00:53:17 -0700

    op3: Add camera shim to always disable QuadraCfa mode
    QuadraCfa mode causes the front-facing camera to fail to take pictures when
    it is enabled. In order to fix the front-facing camera issues, QuadraCfa
    mode must be forcefully disabled.
    
    This shim overrides three key functions related to QuadraCfa, and uses them
    to permanently kill QuadraCfa. In order to make the camera HAL use the shim's
    symbols, a camera HAL hex edit is required.
    
    Change-Id: I0cc5c7f825adb066469ad3defaaee7c29d298765

--- camera/camera_shim.cpp
+++ camera/camera_shim.cpp
@@ -15,4 +15,32 @@
  * limitations under the License.
  */
 
+
+#include <stdint.h>
+
 const char *_ZN7android18gClientPackageNameE;
+
+extern "C" {
+
+// int32_t QCameraParameters::setQuadraCfaMode(uint32_t enable, bool initCommit)
+// _ZN7qcamera17QCameraParameters16setQuadraCfaModeEjb
+int32_t _ZN7qcamera17QCameraParameters16setQuadraCfaModSHIM(uint32_t, bool)
+{
+    return 0;
+}
+
+// int32_t QCameraParameters::setQuadraCfa(const QCameraParameters& params)
+// _ZN7qcamera17QCameraParameters12setQuadraCfaERKS0_
+int32_t _ZN7qcamera17QCameraParameters12setQuadraCfaERSHIM(const void *)
+{
+    return 0;
+}
+
+// bool QCameraParameters::getQuadraCfa()
+// _ZN7qcamera17QCameraParameters12getQuadraCfaEv
+bool _ZN7qcamera17QCameraParameters12getQuadraCSHIM(void)
+{
+    return false;
+}
+
+}
