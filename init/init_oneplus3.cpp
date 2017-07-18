LineageOS/android_device_oneplus_oneplus3

op3: Update from OB19
Change-Id: Ibd41bc900b9b2e26ec71ee2559db5cbb9d35e9b2

init/init_oneplus3.cpp
@@ -106,16 +106,16 @@ void load_op3(const char *model) {
     property_override("ro.product.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override("ro.product.device", "OnePlus3");
-    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 63 dev-keys");
-    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/05151830:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 113 dev-keys");
+    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/06151600:user/release-keys");
 }
 
 void load_op3t(const char *model) {
     property_override("ro.product.model", model);
     property_override("ro.build.product", "OnePlus3");
     property_override("ro.product.device", "OnePlus3T");
-    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 48 dev-keys");
-    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:7.1.1/NMF26F/05151900:user/release-keys");
+    property_override("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 73 dev-keys");
+    property_override("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:7.1.1/NMF26F/06151930:user/release-keys");
 }
 
 void vendor_load_properties() {

Myst3rion09/device_oneplus_oneplus3

 * The updated fingerprint HAL breaks backward compatibility with old
   firmware, and OnePlus didn't update TZ version, so we assert on firmware
   build timestamp instead.
 * Timestamp extracted from OnePlus3Hydrogen_16_OTA_022_all_1706272151_0da875d901b04b85

init/init_oneplus3.cpp
@@ -94,8 +94,8 @@ void load_op3(const char *model) {
     property_set("ro.battery.capacity", "3000");
     property_set("ro.build.product", "OnePlus3");
     property_set("ro.product.device", "OnePlus3");
-    property_set("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 63 dev-keys");
-    property_set("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/05151830:user/release-keys");
+    property_set("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 113 dev-keys");
+    property_set("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3:7.1.1/NMF26F/06151600:user/release-keys");
 }
 
 void load_op3t(const char *model) {
@@ -103,8 +103,8 @@ void load_op3t(const char *model) {
     property_set("ro.battery.capacity", "3400");
     property_set("ro.build.product", "OnePlus3");
     property_set("ro.product.device", "OnePlus3T");
-    property_set("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 48 dev-keys");
-    property_set("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:7.1.1/NMF26F/05151900:user/release-keys");
+    property_set("ro.build.description", "OnePlus3-user 7.1.1 NMF26F 73 dev-keys");
+    property_set("ro.build.fingerprint", "OnePlus/OnePlus3/OnePlus3T:7.1.1/NMF26F/06151930:user/release-keys");
 }
 
 void vendor_load_properties() {